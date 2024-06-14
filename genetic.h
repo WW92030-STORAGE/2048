c716daf59459db6fb4c52feac8d75108320ab2fc
NormalExisting <65929722+WW92030-STORAGE@users.noreply.github.com>
2024-06-14 17:07:33 -0500

    Update genetic.h

--- genetic.h
+++ genetic.h
@@ -15,31 +15,41 @@ class AI2048 {
     double emptytile = 0.5;
 	double rough = -2;
 	double matsum = 4; // Weighted sum
+    double rinv = 1;
+    double flatboard = -10; // Penalty of a flat board (which forces a movement in the "wrong" direction)
+    double trueflat = -100; // Penalty of a 3x4 flat board
     long long INF = 100000000000000; // 1e14
     int ply = 2;
 
     std::string toString() {
         std::string res = "EMPTY " + std::to_string(emptytile) + " ROUGH " + std::to_string(rough) + " MATSUM " + std::to_string(matsum);
+        res = res + " RINV " + std::to_string(rinv) + " FLAT " + std::to_string(flatboard) + " TRUEFLAT " + std::to_string(trueflat);
         return res;
     }
     
     bool operator<(AI2048& o) {
-        std::vector<double> v1 = {emptytile, rough, matsum};
-        std::vector<double> v2 = {o.emptytile, o.rough, o.matsum};
+        std::vector<double> v1 = {emptytile, rough, matsum, rinv, flatboard, trueflat};
+        std::vector<double> v2 = {o.emptytile, o.rough, o.matsum, o.rinv, o.flatboard, o.trueflat};
         return v1 < v2;
     }
     
     AI2048() { }
-    AI2048(double em, double ro, double ma) {
+    AI2048(double em, double ro, double ma, double ri, double fl, double tr) {
         emptytile = em;
         rough = ro;
         matsum = ma;
+        rinv = ri;
+        flatboard = fl;
+        trueflat = tr;
     }
 
 	AI2048(const AI2048& a) {
 		emptytile = a.emptytile;
 		rough = a.rough;
 		matsum = a.matsum;
+        rinv = a.rinv;
+        flatboard = a.flatboard;
+        trueflat = a.trueflat;
 	}
     
     int dx[4] = {01, 00, -1, 00};
@@ -81,11 +91,68 @@ class AI2048 {
 		return res;
 	}
     
+    // This method assumes our desired placement of tiles concentrates in the bottom right. In the wrapper function this is run on various rotated and reflected boards.
     double getOneDirectionScore(Game2048 game) {
         if (game.gameCleared()) return INF;
         if (game.gameOver()) return -1 * INF;
         int empties = 0;
 		int roughness = 0;
+        int rowinv = 0;
+
+        // Flat board calculation
+
+        bool ff1 = true;
+        bool foundthings = false;
+        for (int i = 0; i < 4; i++) {
+            int sum = 0;
+            for (int j = 0; j < 4; j++) sum += (game.board[i][j] > 0) ? 1 : 0;
+            if (sum > 0 && sum < 4) {
+                ff1 = false;
+                break;
+            }
+            if (sum == 4) foundthings = true;
+            if ((sum == 0) && foundthings) {
+                ff1 = false;
+                break;
+            }
+        }
+
+        bool ff2 = true;
+        foundthings = false;
+        for (int i = 0; i < 4; i++) {
+            int sum = 0;
+            for (int j = 0; j < 4; j++) sum += (game.board[j][i] > 0) ? 1 : 0;
+            if (sum > 0 && sum < 4) {
+                ff2 = false;
+                break;
+            }
+            if (sum == 4) foundthings = true;
+            if ((sum == 0) && foundthings) {
+                ff2 = false;
+                break;
+            }
+        }
+
+        bool foundflat = ff1 || ff2;
+
+        // True flatboard calculation
+        bool tf1 = true;
+        for (int i = 0; i < 4; i++) {
+            if (game.board[i][0] > 0) tf1 = false;
+            if (game.board[i][1] == 0) tf1 = false;
+            if (game.board[i][2] == 0) tf1 = false;
+            if (game.board[i][3] == 0) tf1 = false;
+        }
+
+        bool tf2 = true;
+        for (int i = 0; i < 4; i++) {
+            if (game.board[0][i] > 0) tf1 = false;
+            if (game.board[1][i] == 0) tf1 = false;
+            if (game.board[2][i] == 0) tf1 = false;
+            if (game.board[3][i] == 0) tf1 = false;
+        }
+
+        bool foundtrueflat = tf1 || tf2;
         
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
@@ -101,8 +168,14 @@ class AI2048 {
                 }
             }
         }
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (game.board[i][j] > game.board[i][j + 1]) rowinv++;
+            }
+        }
         
-		double retval = emptytile * empties + rough * roughness + matsum * matrixSum(game);
+		double retval = emptytile * empties + rough * roughness + matsum * matrixSum(game) + rinv * rowinv + flatboard * foundflat + trueflat * foundtrueflat;
 
 		return retval;
     }
@@ -196,8 +269,8 @@ class AI2048 {
 };
 
 bool operator<(const AI2048& t, const AI2048& o) {
-    std::vector<double> v1 = {t.emptytile, t.rough, t.matsum,};
-    std::vector<double> v2 = {o.emptytile, o.rough, o.matsum};
+    std::vector<double> v1 = {t.emptytile, t.rough, t.matsum, t.rinv};
+    std::vector<double> v2 = {o.emptytile, o.rough, o.matsum, o.rinv};
     return v1 < v2;
 }
 
@@ -227,7 +300,7 @@ std::vector<AI2048> tournament(std::vector<AI2048> v, int k, bool verbose = true
     std::vector<std::pair<long long, AI2048>> rank;
     for (auto i : v) {
         rank.push_back({-1 * test(i), AI2048(i)});
-        if (verbose) std::cout << "X";
+        if (verbose) std::cout << "X\n";
     }
     std::cout << "\n";
     
@@ -246,15 +319,18 @@ double randrad() {
 }
 
 AI2048 randomAI(double radius = 4096) {
-    return AI2048(radius * randrad(), radius * randrad(), radius * randrad());
+    return AI2048(radius * randrad(), radius * randrad(), radius * randrad(), radius * randrad(), radius * randrad(), radius * randrad());
 }
 
 AI2048 mutate(AI2048 ai, double radius = 16) {
     AI2048 res(ai);
     int blah = rand() % 32;
-    if (blah == 3) res.emptytile = radius * randrad();
-	if (blah == 8) res.rough = radius * randrad();
-	if (blah == 9) res.matsum = radius * randrad();
+    if (blah == 0) res.emptytile = radius * randrad();
+	if (blah == 1) res.rough = radius * randrad();
+	if (blah == 2) res.matsum = radius * randrad();
+    if (blah == 3) res.rinv = radius * randrad();
+    if (blah == 4) res.flatboard = radius * randrad();
+    if (blah == 5) res.trueflat = radius * randrad();
     return res;
 }
 
@@ -263,6 +339,9 @@ AI2048 cross(AI2048 a1, AI2048 a2) {
     if (rand() % 2 == 0) res.emptytile = a2.emptytile;
 	if (rand() % 2 == 0) res.rough = a2.rough;
 	if (rand() % 2 == 0) res.matsum = a2.matsum;
+    if (rand() % 2 == 0) res.rinv = a2.rinv;
+    if (rand() % 2 == 0) res.flatboard = a2.flatboard;
+    if (rand() % 2 == 0) res.trueflat = a2.trueflat;
     return res;
 }
 
@@ -275,5 +354,8 @@ AI2048 cross(AI2048 a1, AI2048 a2) {
 2ply 16x64 3/r
 
 EMPTY 1.772027 ROUGH -1136.909696 MATSUM 14.485305
+EMPTY 7.224121 ROUGH -3988.781414 MATSUM 15.735084 RINV -11.251073
+EMPTY 11.970711 ROUGH -2496.006038 MATSUM 11.952956 RINV 8.097248 FLAT -9.355223
+EMPTY 2739.060733 ROUGH -2793.153255 MATSUM 10.774997 RINV 5.861263 FLAT -14.881667 TRUEFLAT -5.521547
 
 */
